7b44919930e95f4f1a134e2c7a0e21663110a863
dmaf <dmaf.andinofiallos@gmail.com>
2025-06-03 01:42:15 +0200

    previous version of the code

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-//For consistency please write classes in "section--type" formate, for example "card--container", "column--section", "footer-wrapper"
+//For consistency please write classes in "section--type" formate, for example "card--container", "column--section", "footer--wrapper"
 //Please follow this formate so we can have an easier time refactoring later.
